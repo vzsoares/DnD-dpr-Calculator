aa0507df1fd296831cb733f6c491124f99b48f7c
vzsoares <jogolots2@gmail.com>
2022-05-23 20:45:49 -0300

    fea:HitDamageSection States

--- packages/features/calculatorContext.js
+++ packages/features/calculatorContext.js
@@ -1,12 +1,29 @@
 import { createContext, useContext, useMemo, useState } from "react";
+import Attack from "../data/Attack";
 
 const calculatorContext = createContext();
-
 function CalculatorContextProvider({ children }) {
-  const [test, setTest] = useState("TEST");
+  //
+  const [attackBonus, setAttackBonus] = useState("");
+  const [damageBonus, setDamageBonus] = useState("");
+  const [gwmsharp, setGwmsharp] = useState(false);
+  const [name, setName] = useState("");
+  const [damageDice, setDamageDice] = useState("");
+  const [critDice, setCritDice] = useState("");
+  const [advantageModifier, setAdvantageModifier] = useState("");
+  const [critRange, setCritRange] = useState("");
+  const [targetAC, setTargetAC] = useState("");
+  //
   const contextData = useMemo(() => {
-    return { test };
-  }, []);
+    return {
+      attackBonus,
+      gwmsharp,
+      damageBonus,
+      setGwmsharp,
+      setAttackBonus,
+      setDamageBonus,
+    };
+  }, [attackBonus, gwmsharp, damageBonus]);
   return (
     <calculatorContext.Provider value={contextData}>
       {children}
