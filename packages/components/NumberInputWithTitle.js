4008b8a5e658abdbf9712dba6ea9556da46b4ff0
vzsoares <jogolots2@gmail.com>
2022-06-05 10:16:06 -0300

    enh:defaultProps method

--- packages/components/NumberInputWithTitle.js
+++ packages/components/NumberInputWithTitle.js
@@ -11,12 +11,20 @@ import {
 } from "@chakra-ui/react";
 
 export default function NumberInputWithTitle({
-  props: { roll, value, setValue, flexDir, justify, font, size },
+  props: {
+    roll,
+    value,
+    setValue,
+    justify,
+    size,
+    flexDir = "column",
+    font = "1.5rem",
+  },
 }) {
   return (
     <>
-      <Flex flexDir={flexDir || "column"} justify={justify}>
-        <Heading fontSize={font || "1.5rem"}>{roll}</Heading>
+      <Flex flexDir={flexDir} justify={justify}>
+        <Heading fontSize={font}>{roll}</Heading>
         <NumberInput
           maxW='90px'
           value={value}
