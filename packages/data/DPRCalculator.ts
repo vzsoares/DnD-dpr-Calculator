8fdf080b745f7955cfc7ac95f8ab78b4bb7ab04f
lnunes13 <lucianoaugusto13@gmail.com>
2022-07-04 19:10:23 -0400

    added comments

--- packages/data/DPRCalculator.ts
+++ packages/data/DPRCalculator.ts
@@ -11,26 +11,26 @@
 */
 
 class Attack {
-  name: string;
-  attack_bonus: number;
-  damage_bonus: number;
-  damage_dice: DiceSet;
-  crit_dice: DiceSet;
-  advantage_modifier: number;
-  gwmsharp: boolean;
-  crit_range: number;
-  target_AC: number;
+  name: string;                 // custom name for the attack
+  attack_bonus: number;         // attack roll bonus, also known as hit/dc
+  damage_bonus: number;         // damage roll bonus, for example bonus for strength or magic items.
+  damage_dice: DiceSet;         // array of die, for example 2d6 for a greatsword.
+  crit_dice: DiceSet;           // array of die for when attack is critical hit, for example 4d6 for a greatsword plus 6d8 for divine smite used only when attack is a crit
+  advantage_modifier: number;   // 1 for normal, 2 for advantage and 3 for elven accuracy
+  gwmsharp: boolean;            // 0 for no, 1 for yes. adds plus 10 to damage bonus, and reduces 5 from attack bonus.
+  crit_range: number;           // 20 for crit on 20 only. 19 for crit range of 19-20.
+  target_AC: number;            // target's armor class.
 
   constructor(
-    name: string, // string, name of attack
-    attack_bonus: number, // int, attack bonus, including all possible bonuses
-    damage_bonus: number, // int, damage bonus, including all possible bonuses
-    damage_dice: DiceSet, // DiceSet.
-    crit_dice: DiceSet, // DiceSet.
-    advantage_modifier: number, // int, set 1 for normal, set 2 for  advantage, set 3 for elven accuracy.
-    gwmsharp: boolean, // int, Great Weapon Master, or Sharpshooter feat. set 0 for false, 1 for true.
-    crit_range: number, // int, use n for 18-20, or 19 for 19-20, or 20 for standard crit chance.
-    target_AC: number // int, the target's armor class
+    name: string,               
+    attack_bonus: number,      
+    damage_bonus: number,      
+    damage_dice: DiceSet, 
+    crit_dice: DiceSet, 
+    advantage_modifier: number, 
+    gwmsharp: boolean, 
+    crit_range: number, 
+    target_AC: number
   ) {
     this.name = name;
     this.attack_bonus = attack_bonus;
@@ -43,6 +43,7 @@ class Attack {
     this.target_AC = target_AC;
   }
 
+  // Calculates the effective attack bonus to account for great weapon master (or sharpshooter).
   getEffectiveAttackBonus() {
     if (this.gwmsharp == true) {
       return this.attack_bonus - 5;
@@ -50,6 +51,7 @@ class Attack {
     return this.attack_bonus;
   }
 
+  // Calculates the effective damage bonus to account for great weapon master (or sharpshooter).
   getEffectiveDamageBonus() {
     if (this.gwmsharp == true) {
       return this.damage_bonus + 10;
@@ -57,6 +59,9 @@ class Attack {
     return this.damage_bonus;
   }
 
+  // Returns the average damage from dice only ACCOUNTING FOR CHANCE TO HIT.
+  // For example, if your chance to hit is 65% and you are attacking with a greatsword with advantage,
+  // your average damage will be (1 - (1 - 0.65) ^ 2) * (2 * 3.5) = 6.1425 
   getAverageFromDice() {
     return (
       p_hit(
@@ -67,6 +72,9 @@ class Attack {
     );
   }
 
+  // Calculates the average damage from bonus only, ACCOUNTING FOR CHANCE TO HIT.
+  // For example if chance to hit is 65% and you have a damage bonus of 5 and you are attacking with elven accuracy,
+  // your damage will be (1 - (1 - 0.65) ^ 3) * 5 = 4.785625
   getAverageFromBonus() {
     return (
       p_hit(
@@ -77,6 +85,14 @@ class Attack {
     );
   }
 
+  // Returns the average damage PER TURN derived from critical hits. 
+  // THIS IS NOT A CALCULATION OF THE AVERAGE DAMAGE DEALT BY A CRITICAL HIT.
+  // For example if chance to hit is 65%, you are attacking with a greatsword, you have advantage, 
+  // you crit on 19-20, and you use 3rd level smite only on crits.
+  // Your chance to crit will then be 1 - (1 - 0.10) ^ 2 = 0.19 = 19%
+  // To find the damage you multiply the chance to crit by all the EXTRA DAMAGE that you would get from the attack.
+  // the EXTRA DAMAGE of this attack would be (2 * 3.5 + 6 * 4.5) = 34
+  // The final result returned will then be 0.19 * 34 = 6.46
   getAverageFromCritFactor() {
     return (
       p_crit(this.crit_range, this.advantage_modifier) *
@@ -84,6 +100,7 @@ class Attack {
     );
   }
 
+  // Returns the average damage dealt by the attack accounting for average dice rolls, bonus, critical hits, and change to hit
   getAverageTotal() {
     return (
       this.getAverageFromDice() +
@@ -100,6 +117,7 @@ class Attack {
     This class is used to instantiate DieRoll objects,
     which represent a single dice roll with along with
     modifiers for rerolling and minimum rolls.
+    NOTE: Rerolling more than once is not supported.
 */
 
 class Die {
@@ -113,7 +131,7 @@ class Die {
   }
 
   // Returns the average roll for a die
-  // given its number of sides, minimum value to reroll once, and minimum roll.
+  // given its number of sides, minimum value to reroll ONCE, and minimum roll.
   getAverageRoll(): number {
     if (this.reroll == 0) {
       return (T(this.sides) + T(this.minRoll - 1)) / this.sides;
@@ -208,21 +226,105 @@ function p_crit(crit_range: number, adv_mod: number): number {
 //-------------------//
 //--- TEST SCRIPT ---//
 //-------------------//
-
 /*let my_attack = new Attack(
     "Holy Attack",
     9,
     5,
-    new DiceSet([ { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905668944 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669096 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669215 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669341 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669453 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669573 }, { "sides": 8, "reroll": 1, "minRoll": 1, "id": 1654905669813 }, { "sides": 8, "reroll": 1, "minRoll": 1, "id": 1654905669927 } ]),
-    new DiceSet([ { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905668944 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669096 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669215 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669341 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669453 }, { "sides": 12, "reroll": 1, "minRoll": 1, "id": 1654905669573 }, { "sides": 8, "reroll": 1, "minRoll": 1, "id": 1654905669813 }, { "sides": 8, "reroll": 1, "minRoll": 1, "id": 1654905669927 } ]),
-    1,
-    false,
+    new DiceSet([
+      {
+          "sides": 6,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967414633
+      },
+      {
+          "sides": 6,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967414867
+      }
+  ]),
+    new DiceSet([
+      {
+          "sides": 6,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967445367
+      },
+      {
+          "sides": 6,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967445501
+      },
+      {
+          "sides": 6,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967445678
+      },
+      {
+          "sides": 6,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967445828
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967455705
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967455896
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967456243
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967456903
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967457546
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967457819
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967458124
+      },
+      {
+          "sides": 8,
+          "reroll": 0,
+          "minRoll": 1,
+          "id": 1654967458564
+      }
+  ]),
+    2,
+    true,
     20,
     15
 )
 
-console.log(my_attack.crit_dice.getAverageRolls());
-console.log("\n          Chance to Hit: " + p_hit(15, 9, 1));
+console.log("\n          Chance to Hit: " + p_hit(15, 4, 2));
 console.log("       Damage From Dice: " + my_attack.getAverageFromDice());
 console.log("      Damage From Bonus: " + my_attack.getAverageFromBonus());
 console.log("Damage From Crit Factor: " + my_attack.getAverageFromCritFactor());
