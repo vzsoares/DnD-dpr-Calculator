ff6f66eea236e3c4d474e5e2370ab266b1c7f873
vzsoares <jogolots2@gmail.com>
2022-07-10 19:40:22 -0300

    fix max height

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,29 @@ import { ChakraProvider, Box } from "@chakra-ui/react";
 
 import "../styles/globals.css";
 import NavBar from "../packages/components/NavBar.js";
+import { useEffect, useRef, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const navbar = useRef();
+  const [navbarHeight, setNavbarHeight] = useState(58);
+  useEffect(() => {
+    setNavbarHeight(navbar.current.clientHeight);
+  }, []);
   return (
     <ChakraProvider>
       {/* @ts-ignore */}
       <CalculatorContextProvider>
-        <Box>
-          <NavBar />
-        </Box>
-        <Box
-          padding='0.5rem'
-          minH='100vh'
-          bgImg="url('/img/background_texture.png')"
-        >
-          <Component {...pageProps} />
+        <Box minH='100vh'>
+          <Box ref={navbar}>
+            <NavBar />
+          </Box>
+          <Box
+            padding='0.5rem'
+            minH={`calc(100vh - ${navbarHeight + 1}px)`}
+            bgImg="url('/img/background_texture.png')"
+          >
+            <Component {...pageProps} />
+          </Box>
         </Box>
       </CalculatorContextProvider>
     </ChakraProvider>
