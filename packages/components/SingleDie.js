193499e1cc1f8e5a5ec2ccdc54a47eae8a5d0f43
vzsoares <jogolots2@gmail.com>
2022-07-12 14:18:16 -0300

    add dices icons

--- packages/components/SingleDie.js
+++ packages/components/SingleDie.js
@@ -1,29 +1,29 @@
 import { Box } from "@chakra-ui/react";
+import { Dice10, Dice12, Dice4, Dice6, Dice8, DiceN } from "../icons/Dices";
 export default function SingleDie({ props: { value, func, color } }) {
   function handleClick(e) {
     e.preventDefault();
     func();
   }
+  const icons = {
+    0: DiceN,
+    4: Dice4,
+    6: Dice6,
+    8: Dice8,
+    10: Dice10,
+    12: Dice12,
+  };
   return (
-    <Box h='40px' w='40px' bgColor={color || "green"} border='2px solid black'>
+    <Box h='40px' w='40px' bgColor={color || ""} borderRadius='15px'>
       <a
         style={{
           cursor: "pointer",
         }}
         onClick={(e) => handleClick(e)}
       >
-        <div
-          style={{
-            height: "100%",
-            width: "100%",
-            textAlign: "center",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <h1 style={{ fontWeight: "bolder" }}>{value.sides ?? value}</h1>
-        </div>
+        {icons[value.sides ?? value]
+          ? icons[value.sides ?? value]()
+          : icons[0]()}
       </a>
     </Box>
   );
